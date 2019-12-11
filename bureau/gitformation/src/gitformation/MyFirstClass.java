8f9b5d21b424c79104c3b25bbfa35718a5234d94
htaffard <htaffard@L10FRNDC01LE171.eua.bvcorp.corp>
2019-12-11 06:23:13 +0100

    working on feature 1

--- bureau/gitformation/src/gitformation/MyFirstClass.java
+++ bureau/gitformation/src/gitformation/MyFirstClass.java
@@ -1,8 +1,12 @@
 package gitformation;
 
+import java.util.Date;
+
 public class MyFirstClass {
 
     public void travelToNoida() {
-        
+        int hours;
+        Date timeOfDeparture;
+        Date timeOfArrival;
     }
 }
