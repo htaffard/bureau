a42e8a80300084b60003af0abd1d140a1ba3fb35
htaffard <htaffard@L10FRNDC01LE171.eua.bvcorp.corp>
2019-12-13 07:14:28 +0100

    message 3

--- training/training/src/training/TravelToNoida.java
+++ training/training/src/training/TravelToNoida.java
@@ -1,9 +1,11 @@
 package training;
 
+import java.util.Date;
+
 public class TravelToNoida {
 
     public void travelToNoida() {
-        
+       Date timeOfArrival = new Date(); 
     }
     
     public void meetTheTeam() {
